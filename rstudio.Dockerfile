5e5aecb33548b26a0593fd98cb4e743348d95247
lorransr <lorran.rodr@gmail.com>
2023-05-09 23:47:49 -0300

    fix: m1 mac build

--- rstudio.Dockerfile
+++ rstudio.Dockerfile
@@ -1,7 +1,17 @@
-FROM rocker/rstudio
+FROM rocker/r-ver:4.2.1
+
+ENV S6_VERSION=v2.1.0.2
+ENV RSTUDIO_VERSION=daily
+ENV DEFAULT_USER=rstudio
+ENV PANDOC_VERSION=default
+ENV PATH=/usr/lib/rstudio-server/bin:$PATH
+
+RUN /rocker_scripts/install_rstudio.sh
+RUN /rocker_scripts/install_pandoc.sh
+
+COPY scripts/install-packages.R install-packages.R
+RUN Rscript install-packages.R
 
 EXPOSE 8787
 
-COPY scripts/install-packages.R install-packages.R
-
-RUN Rscript install-packages.R
+CMD ["/init"]
--- scripts/install-packages.R
+++ scripts/install-packages.R
@@ -1,22 +1,22 @@
 installa_load_packages <- function(pkg){
   new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
-  if (length(new.pkg)) 
+  if (length(new.pkg))
     install.packages(new.pkg, dependencies = TRUE)
   sapply(pkg, require, character.only = TRUE)
 }
 
 # lista de pacotes necessários
-packages <- c("sparklyr", 
+packages <- c("sparklyr",
              "ggcorrplot",
-             "dplyr", 
+             "dplyr",
              "caret",
              "nlme",
-             "ggplot2", 
-             "carrier", 
-             "mlflow", 
-             "reticulate", 
-             "stats", 
+             "ggplot2",
+             "carrier",
+             "mlflow",
+             "reticulate",
+             "stats",
              "glue")
 
 # instalar e carregar os pacotes
-installa_load_packages(pacotes)
+installa_load_packages(packages)
